069759ea3e78eecb42fdabe079b671f645971163
Alexander Gorban <gorban.alexander.y@gmail.com>
2016-11-14 16:31:09 +0200

    SignalR. PerfServer.

--- PerfSurf/Hubs/PerfHub.cs
+++ PerfSurf/Hubs/PerfHub.cs
@@ -1,9 +1,30 @@
 ﻿using Microsoft.AspNet.SignalR;
+using PerfSurf.Counters;
+using System.Threading.Tasks;
 
 namespace PerfSurf.Hubs
 {
     public class PerfHub : Hub
     {
+        public PerfHub()
+        {
+            StartCounterCollection();
+        }
+
+        private void StartCounterCollection()
+        {
+            var task = Task.Factory.StartNew(async () =>
+            {
+                var perfService = new PerfCounterService();
+                while (true)
+                {
+                    var result = perfService.GetResults();
+                    Clients.All.newCounters(result);
+                    await Task.Delay(2000);
+                }
+            });
+        }
+
         public void Send(string message)
         {
             Clients.All.newMessage(
